4471441d062f597b118b19451bd6c104a3642cca
safvancmc <safvan.cmc@tekgile.com>
2025-12-07 19:15:46 +0530

    base64 secretkey

--- secrete.js
+++ secrete.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
 // Generate a random secret key with 32 bytes length
-const secretKey = crypto.randomBytes(32).toString('hex');
+const secretKey = crypto.randomBytes(32).toString('base64');
 
 console.log("Generated Secret Key:", secretKey);
